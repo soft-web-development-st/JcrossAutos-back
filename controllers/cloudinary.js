485cc2e493e79555dc3c9faa23329c01bf94f28d
soft.web.development <softwebdevelopment@gmail.com>
2022-03-14 09:10:11 +0100

    second

--- controllers/cloudinary.js
+++ controllers/cloudinary.js
@@ -1,30 +1,28 @@
-const cloudinary = require('cloudinary')
+const cloudinary = require("cloudinary");
 
 //config
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUDE_NAME,
-    api_key :process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET
-})
-
-
+  cloud_name: process.env.CLOUDINARY_CLOUDE_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 exports.upload = async (req, res) => {
-    let result = await cloudinary.uploader.upload_large(req.body.image, {
-        public_id: `${Date.now()}`,
-        resource_type: 'auto'
-    });
-    res.json({
-        public_id: result.public_id,
-        url: result.secure_url,
-    })
-}
+  let result = await cloudinary.v2.uploader.upload(req.body.image, {
+    public_id: `${Date.now()}`,
+    resource_type: "auto",
+  });
+  res.json({
+    public_id: result.public_id,
+    url: result.secure_url,
+  });
+};
 
-exports.remove =  (req, res) => { 
-    let image_id = req.body.public_id
-    cloudinary.uploader.destroy(image_id, (err, result) => {
-        if (err) return res.json({ success: false, err });
-        res.send('Deleted Succefully')
-    })
-}
+exports.remove = (req, res) => {
+  let image_id = req.body.public_id;
+  cloudinary.uploader.destroy(image_id, (err, result) => {
+    if (err) return res.json({ success: false, err });
+    res.send("Deleted Succefully");
+  });
+};
